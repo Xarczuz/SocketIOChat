04d6d3627b517c20805767af9611553f8469224e
Chjun-chi Chiu <chjunchi.chiu@gmail.com>
2018-11-25 23:40:56 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  rules: {
+    indent: ["error", 2],
+    "no-var": ["error"],
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"]
+  }
+};
---
+++ index.js
@@ -0,0 +1,77 @@
+const app = require('express')();
+const express = require('express');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+// let io = require('socket.io')(http, {
+//   serveClient: true,
+//   // below are engine.IO options
+//   pingInterval: 3000,
+//   pingTimeout: 10000,
+//   cookie: false
+
+// });
+process.on('uncaughtException', function (err) {
+  console.error('uncaughtException', err.stack);
+
+});
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+app.use(express.static(__dirname + '/public'));
+
+let userList = { users: [] };
+
+io.on('connection', function (socket) {
+  console.log('HEJ');
+  let userName = '';
+  sendUserList(userList);
+  socket.on('change_username', function (data) {
+    {
+      userName = data.username;
+      socket.username = userName;
+      userList.users.push(userName);
+      sendUserList(userList);
+      io.emit('chat message', socket.username + ' connected');
+      console.log(socket.username + ' connected');
+    }
+  });
+  socket.on('sendPing', function (userName) {
+    let d = new Date();
+    console.log(d + userName + ' pinged');
+  });
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+  socket.on('done typing', function () {
+    socket.broadcast.emit('done typing', {
+      username: socket.username
+    });
+  });
+  socket.on('disconnect', function () {
+    let index = userList.users.indexOf(userName);
+    if (index > -1) {
+      userList.users.splice(index, 1);
+    }
+    sendUserList(userList);
+    console.log(socket.username + ' user disconnected');
+    socket.username = '';
+  });
+
+  socket.on('chat message', function (msg) {
+    console.log(socket.username + ': ' + msg);
+    io.emit('chat message', socket.username + ': ' + msg);
+  });
+});
+
+http.listen(3000, function () {
+  console.log('listening on *:3000');
+});
+
+function sendUserList(userList) {
+  io.emit('userList', userList);
+}
